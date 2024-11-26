6add739cfe0bae25ceb37862930cb5cbe71dcd8d
toxabrat <toxabrat97@mail.ru>
2024-11-26 16:17:48 +0300

    add RowFactory

--- table/RowFactory.cpp
+++ table/RowFactory.cpp
@@ -1,1 +1,12 @@
 #include "RowFactory.hpp"
+
+std::shared_ptr<Row> RowFactory::concatenateRow(const std::shared_ptr<Row>& leftRow, const std::shared_ptr<Row>& rightRow) {
+    std::vector<std::shared_ptr<Object>> fields;
+    for(auto &i : leftRow -> getField()) {
+        fields.push_back(i);
+    }
+    for(auto &i : rightRow -> getField()) {
+        fields.push_back(i);
+    }
+    return std::make_shared<Row>(fields);
+}
