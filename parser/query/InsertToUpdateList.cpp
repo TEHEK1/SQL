8b621a80adb2c3f003eaae808e77bd3a7e5cf14d
TEHEK1 <kasapovamir9@gmail.com>
2024-11-26 19:19:51 +0300

    InsertToUpdateList.cpp

--- parser/query/InsertToUpdateList.cpp
+++ parser/query/InsertToUpdateList.cpp
@@ -1,15 +1,15 @@
 #include "InsertToUpdateList.hpp"
 #include "TableFactory.hpp"
 
-InsertToUpdateList::InsertToUpdateList(const InsertUpdateList & updateEqualList,
+InsertToUpdateList::InsertToUpdateList(const UpdateList & updateList,
                                        const std::shared_ptr<Relation> & relation) :
-                                       m_updateEqualList(updateEqualList), m_relation(relation) {}
+                                       m_updateList(updateList), m_relation(relation) {}
 
 bool InsertToUpdateList::executeQuery(const std::shared_ptr<DataBase> & dataBase) const {
     auto table = m_relation->getTableRelation(dataBase);
     auto tableMeta = table->getTableMeta();
     TableFactory::InsertOperatorList operatorList(tableMeta.size());
-    for(const auto& [columnName, operatorResult] : m_updateEqualList) {
+    for(const auto& [columnName, operatorResult] : m_updateList) {
         operatorResult->getObjectOperator(std::shared_ptr<Row>(), TableMeta());
         operatorList[tableMeta.getByName(columnName)->getRealColumnNum()] = operatorResult;
     }
--- parser/query/InsertToUpdateList.hpp
+++ parser/query/InsertToUpdateList.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include <vector>
 #include "InsertTo.hpp"
-#include <UpdateEqualList.hpp>
+#include "UpdateEqualList.hpp"
 
 class InsertToUpdateList : public InsertTo {
 public:
-    typedef std::vector<std::pair<std::string, std::shared_ptr<Operator> > > InsertUpdateList;
-    InsertToUpdateList(const InsertUpdateList&, const std::shared_ptr<Relation>&);
+    InsertToUpdateList(const UpdateList&, const std::shared_ptr<Relation>&);
     bool executeQuery(const std::shared_ptr<DataBase> &) const override;
 private:
-    InsertUpdateList m_updateEqualList;
+    UpdateList m_updateList;
     std::shared_ptr<Relation> m_relation;
 };
