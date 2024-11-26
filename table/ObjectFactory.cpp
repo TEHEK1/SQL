4fcb123d8291fafd63f67a468536278340dfa975
toxabrat <toxabrat97@mail.ru>
2024-11-26 16:11:18 +0300

    add ObjectFactory.cpp

--- table/ObjectFactory.cpp
+++ table/ObjectFactory.cpp
@@ -1,5 +1,5 @@
 #include "ObjectFactory.hpp"
 
-std::shared_ptr<Object> ObjectFactory::getObjectByColumnName(const std::string&, const std::shared_ptr<Row> &, const TableMeta &) {
-    
+std::shared_ptr<Object> ObjectFactory::getObjectByColumnName(const std::string& columnName, const std::shared_ptr<Row> &row, const TableMeta &meta) {
+    return row -> getField(meta.getByName(columnName) -> getRealColumnNum());
 }
