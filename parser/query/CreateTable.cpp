ff63cf844950f13fd2a8938ca157ffdb0b4055b9
TEHEK1 <kasapovamir9@gmail.com>
2024-11-26 19:11:36 +0300

    CreateTable

---
+++ parser/query/CreateTable.cpp
@@ -0,0 +1,7 @@
+#include "CreateTable.hpp"
+
+CreateTable::CreateTable(const std::string & tableName, const TableMeta & tableMeta) : m_tableName(tableName), m_tableMeta(tableMeta) {}
+
+bool CreateTable::executeQuery(const std::shared_ptr<DataBase> & dataBase) const {
+    dataBase->insertTable(m_tableName, std::make_shared<Table>(m_tableMeta));
+}
---
+++ parser/query/CreateTable.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Query.hpp"
+#include "TableMeta.hpp"
+
+class CreateTable : public Query {
+public:
+    CreateTable(const std::string&, const TableMeta& );
+    bool executeQuery(const std::shared_ptr<DataBase> &) const override;
+private:
+    std::string m_tableName;
+    TableMeta m_tableMeta;
+};
